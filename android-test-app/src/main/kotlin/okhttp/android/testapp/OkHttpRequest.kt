f30274a340046cedd8200a727fd0b7de69732d87
zhouheng <zhouheng@tngdigital.com>
2024-05-15 15:51:34 +0800

    native cache

--- android-test-app/src/main/kotlin/okhttp/android/testapp/OkHttpRequest.kt
+++ android-test-app/src/main/kotlin/okhttp/android/testapp/OkHttpRequest.kt
@@ -3,6 +3,7 @@ package okhttp.android.testapp
 import android.content.Context
 import android.util.Log
 import java.io.File
+import java.io.IOException
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -12,30 +13,43 @@ class OkHttpRequest {
   val TAG = "OkHttpRequest"
   fun testCache(context: Context) {
 
-    val cacheFile = File(context.externalCacheDirs.toString(), "okhttp_cache")
-    val cache = Cache(cacheFile, 10 * 1024 * 1024)
+    Thread {
+      val client: OkHttpClient = OkHttpClient.Builder()
+        .cache(
+          Cache(
+            directory = context.cacheDir,
+            maxSize = 10L * 1024L * 1024L // 10 MiB
+          )
+        )
+        .build()
 
-    val client = OkHttpClient.Builder().cache(cache).build()
+      val request = Request.Builder()
+        .url("https://publicobject.com/helloworld.txt")
+        .build()
 
-    val request = Request.Builder().url("https://publicobject.com/helloworld.txt").build()
-    val call1 = client.newCall(request)
-    val response1 = call1.execute()
+      val response1Body = client.newCall(request).execute().use {
+        if (!it.isSuccessful) throw IOException("Unexpected code $it")
 
-    try {
-      Log.i(TAG, "testCache: response1 : " + response1.body.string())
-      Log.i(TAG, "testCache: response1 cache : " + response1.cacheResponse)
-      Log.i(TAG, "testCache: response1  network: " + response1.networkResponse)
+        println("Response 1 response:          $it")
+        println("Response 1 cache response:    ${it.cacheResponse}")
+        println("Response 1 network response:  ${it.networkResponse}")
+        return@use it.body?.string()
+      }
+      println("Response 1 response:          $response1Body")
 
 
-      val call2 = client.newCall(request)
-      val response2 = call2.execute()
-      Log.i(TAG, "testCache: response2 : " + response2.body.string())
-      Log.i(TAG, "testCache: response2 cache : " + response2.cacheResponse)
-      Log.i(TAG, "testCache: response2  network: " + response2.networkResponse)
-    } catch (e: Exception) {
-      e.printStackTrace()
-    }
+      val response2Body = client.newCall(request).execute().use {
+        if (!it.isSuccessful) throw IOException("Unexpected code $it")
 
+        println("Response 2 response:          $it")
+        println("Response 2 cache response:    ${it.cacheResponse}")
+        println("Response 2 network response:  ${it.networkResponse}")
+        return@use it.body?.string()
+      }
+      println("Response 2 response:          $response2Body")
+
+      println("Response 2 equals Response 1? " + (response1Body == response2Body))
+    }.start()
 
   }
 }
